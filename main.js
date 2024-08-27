412f9e2169d1209f6e588ed929439d6832d42ba2
FIRDAVS00222 <shoirajamilova5@gmail.com>
2024-08-27 13:50:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let ism = document.getElementById('ism');
+let familiya = document.getElementById('familiya');
+let yosh = document.getElementById('yosh')
+
+
+let ismValue = prompt('Ismingizni kiriting...');
+let familiyaValue = prompt('Familiyangizni kiriting...');
+let yoshValue = prompt ('Yoshingizni kiriting')
+let kichik = prompt('1)katta' + '2)kichik' )
+
+ism.textContent = ismValue;
+familiya.textContent = familiyaValue;
+yosh.textContent = yoshValue
